b8f4eead4a641e70bd139add1f4c6d7ed132c754
solik003 <malugasola@gmail.com>
2025-03-06 16:19:43 +0200

    change config

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-// src/config.ts
-
-export const BASE_URL = "http://localhost:5000";
---
+++ src/config/filterOptions.ts
@@ -0,0 +1,5 @@
+export const filterOptions = [
+    { value: "all", label: "Show All" },
+    { value: "active", label: "Show Active" },
+    { value: "deactivated", label: "Show Deactivated" }
+];
